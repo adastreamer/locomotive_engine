9a3acf14d216001916118d58f1a5fa41148385fc
Eugene Khashin <adastreamer@gmail.com>
2015-09-19 04:45:19 +0300

    locomotive install

---
+++ config/initializers/dragonfly.rb
@@ -0,0 +1,22 @@
+# Configure
+Dragonfly.app(:engine).configure do
+  plugin :imagemagick,
+    convert_command:  `which convert`.strip.presence || '/usr/local/bin/convert',
+    identify_command: `which identify`.strip.presence || '/usr/local/bin/identify'
+
+  verify_urls true
+
+  secret '77a30650392b2b557ac0d535a7981814a607c51264874ba0065af404023005ef'
+
+  url_format '/images/dynamic/:job/:basename.:ext'
+
+  fetch_file_whitelist /public/
+
+  fetch_url_whitelist /.+/
+end
+
+# Logger
+Dragonfly.logger = Rails.logger
+
+# Mount as middleware
+Rails.application.middleware.use Dragonfly::Middleware, :engine
